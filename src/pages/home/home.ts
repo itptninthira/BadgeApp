0bf40f76f6d26b3efb2dfc4e600f5cc2611e4cc7
ainthira noo <ninthira@ptnicec.ac.th>
2018-11-15 14:54:07 +0700

    send project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: Array<{id:number,name:string}> = [];
 
-  }
+  constructor(public navCtrl: NavController) {
+    this.items.push({id: 1, name:'test1'});
+    this.items.push({id: 2, name:'test2'});
+    this.items.push({id: 3, name:'test3'});
+    this.items.push({id: 4, name:'test4'});
+    this.items.push({id: 5, name:'test5'});
+}
 
 }
